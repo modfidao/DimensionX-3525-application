c9c47994fa78df7ec02a91ce8622b3cbfbe22daa
kasodesyn <bokang.qian@desyn.io>
2022-11-13 19:55:44 +0800

    add vault config

--- contracts/Vault/Vault.sol
+++ contracts/Vault/Vault.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "./VaultConfig.sol";
+import "./VaultConfig.sol";
 import "../Manager/IManager.sol";
 
-contract Vault {
+contract Vault is VaultConfig {
     uint public contractBalance;
     uint public shareSupply;
     IManager public Manager;
@@ -23,30 +23,35 @@ contract Vault {
     constructor(uint shareSupply_, address Manager_){
         shareSupply = shareSupply_;
         Manager =  IManager(Manager_);
+        manager = msg.sender;
     }
 
-    event ClaimManagerFee(address indexed caller,uint amount);
+    event ClaimManagerFeeForPlatform(address indexed caller,uint amount);
+    event ClaimManagerFeeForProject(address indexed caller,uint amount);
     event ClaimUserReward(address indexed caller, uint amount);
 
     function userWithdrew() external lock {
         address user = msg.sender;
         uint withdrewAmount = this.userCouldRewardTotal(user);
 
-        uint manangerWithdrewAmount = Manager.manageFee() * withdrewAmount / 10**18;
-        uint userWithdrewAmount = withdrewAmount - manangerWithdrewAmount;
+        uint manangerForPlatformWithdrewAmount = Manager.manageFee() * withdrewAmount / 10**18;
+        uint manangerForProjectWithdrewAmount = manageFee * withdrewAmount / 10**18;
+        uint userWithdrewAmount = withdrewAmount - manangerForPlatformWithdrewAmount - manangerForProjectWithdrewAmount;
 
         require(userWithdrewAmount>0,"ERR_NOT_REWARD");
 
         (bool isUserSuccess, ) = user.call{value: userWithdrewAmount}("");
-        (bool isManagerSuccess, ) = Manager.manager().call{value: manangerWithdrewAmount}("");
+        (bool isManagerForPlatformSuccess, ) = Manager.manager().call{value: manangerForPlatformWithdrewAmount}("");
+        (bool isManagerProjectSuccess, ) = manager.call{value: manangerForProjectWithdrewAmount}("");
 
         userPools[user].hasWithdrew += userWithdrewAmount;
         userPools[user].hasWithdrewTimes++;
 
-        emit ClaimManagerFee(user,manangerWithdrewAmount);
+        emit ClaimManagerFeeForPlatform(user,manangerForPlatformWithdrewAmount);
+        emit ClaimManagerFeeForProject(user, manangerForProjectWithdrewAmount);
         emit ClaimUserReward(user,userWithdrewAmount);
 
-        require(isUserSuccess && isManagerSuccess,"ERR_WITHDREW_FAILED");
+        require(isUserSuccess && isManagerForPlatformSuccess && isManagerProjectSuccess,"ERR_WITHDREW_FAILED");
     }
 
     // user could reward total
--- contracts/Vault/VaultConfig.sol
+++ contracts/Vault/VaultConfig.sol
@@ -1,29 +1,29 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// contract VaultConfig {
-//     address public manager;
-//     uint public manageFee; // 10**18 = 100%
+contract VaultConfig {
+    address public manager;
+    uint public manageFee; // 10**18 = 100%
 
-//     // 移交manager
-//     event TransferManager(address from, address to);
-//     // 修改管理费
-//     event ChangedManageFee(uint older, uint renew);
-//     // 获得管理费
-//     event ClaimManagerFee(address indexed caller,uint amount);
+    // 移交manager
+    event TransferManager(address from, address to);
+    // 修改管理费
+    event ChangedManageFee(uint older, uint renew);
+    // 获得管理费
+    event ClaimManagerFee(address indexed caller,uint amount);
 
-//     function _setManager(address manager_) internal onlyManager {
-//         emit TransferManager(manager, manager_);
-//         manager = manager_;
-//     }
+    function _setManager(address manager_) internal onlyManager {
+        emit TransferManager(manager, manager_);
+        manager = manager_;
+    }
 
-//     function changeManageFee(uint manageFee_) external onlyManager {
-//         emit ChangedManageFee(manageFee, manageFee_);
-//         manageFee = manageFee_;
-//     }
+    function changeManageFee(uint manageFee_) external onlyManager {
+        emit ChangedManageFee(manageFee, manageFee_);
+        manageFee = manageFee_;
+    }
 
-//     modifier onlyManager() {
-//         require(msg.sender == manager, "ERR_NOT_OWNER");
-//         _;
-//     }
-// }
+    modifier onlyManager() {
+        require(msg.sender == manager, "ERR_NOT_OWNER");
+        _;
+    }
+}
