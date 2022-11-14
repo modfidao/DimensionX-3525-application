f1c3ebb2b8d607c5d3796422c2d8bfae23c9f2b5
kasodesyn <bokang.qian@desyn.io>
2022-11-14 10:44:49 +0800

    init test

---
+++ test/deploy/mananger.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat")
+const ethers = hre.ethers;
+
+async function main(params) {
+    const Manager = await ethers.getContractFactory("Manager");
+    const ManagerInstance = await Manager.deploy();
+
+    await ManagerInstance.deployed()
+
+    console.log("Manager deployed.")
+
+    return ManagerInstance;
+}
+
+module.exports =  main;
+// main().catch((error) => {
+//     console.error(error);
+//     process.exitCode = 1;
+//   });
+  
